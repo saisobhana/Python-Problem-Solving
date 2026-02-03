c9b732d5ee61eebaa9ea10bcdd0f04dad159ad55
saisobhana <saisobhana123@gmail.com>
2026-02-03 17:03:01 +0530

    rock, paper, scissor game

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -1,13 +1,13 @@
 import random
 is_continue = True
 while is_continue:
-    user_input = input("To roll the Dice please enter Y : If you don't want to roll the dice please enter N : ")
-    if user_input == "Y" or user_input == "y":
+    user_input = input("To roll the Dice please enter Y : If you don't want to roll the dice please enter N : ").upper()
+    if user_input == "Y":
         a = random.randint(1,6)
         b = random.randint(1,6)
         print("First Random Number is",a)
         print("Second Random Number is",b)
-    elif user_input == "N" or user_input == "n":
+    elif user_input == "N":
         print("Thank you for Playing")
         is_continue = False
     else:
